c44e8b34e4f02a9758f0020f2d9c0ff1be24ac13
anderson-incript <anderson.coelho@citechnology.com.br>
2023-10-26 18:37:49 -0300

    0

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,16 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: ['svg', ...getDefaultConfig(__dirname).resolver.assetExts],
-  },
-});
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
